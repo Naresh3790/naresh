d369f809a3d6393d6e3ef4a493ef0551d938a084
Naresh3790 <gantanaresh89@gmail.com>
2026-02-04 16:45:59 +0530

    Remove unused imports from app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from transformers import pipeline
 from textblob import TextBlob
 from nltk.sentiment import SentimentIntensityAnalyzer
 import nltk
-from PyPDF2 import pdfReader  #<..important fix
-import docx
 from collections import Counter
 
 # ------------------ Setup ------------------
